90a38600c3e4772d6766580576f715c70397a81a
jordihofc <jordi.silva@ufv.br>
2021-06-04 12:15:01 -0300

    setup

---
+++ src/main/java/br/com/zupedu/propostasComNoSql/PropostasComNoSqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.propostasComNoSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostasComNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostasComNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupedu/propostasComNoSql/PropostasComNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.propostasComNoSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasComNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
